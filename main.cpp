4a7314b28acc9e4313aed1113268a3e3595c1a0e
sherbzy <lsherburne@mines.edu>
2023-08-06 15:13:19 -0700

    Refactor: create InputOutputPractice class to clean up main.cpp

--- main.cpp
+++ main.cpp
@@ -9,12 +9,8 @@
  */
 
 #include <iostream>
-#include <fstream>
+#include "InputOutputPractice.h";
 using namespace std;
-
-// globals
-string POWELL_FACTS_FILENAME = "text/powell_facts.txt";
-string JOKES_FILENAME = "text/list_jokes.txt";
 
 // function declarations
 bool readFileToStdout();
@@ -23,71 +19,13 @@ bool printStdinToFile();
 int main() {
     cout << "Hello, World!" << endl;
 
-    // read from file and output to stdout
+    // practice reading from and writing to files
     cout << "\n\n\n1. Practice reading from and writing to files." << endl;
-    if (!readFileToStdout()) {
-        cout << "Terminating program...\nGoodbye." << endl;
-        return -1;
-    }
+    InputOutputPractice io;
 
-    // read from stdin and output to file
-    if (!printStdinToFile()) {
-        cout << "Terminating program...\nGoodbye." << endl;
-        return -1;
-    }
-
+    // sting practice
+    cout << "\n\n\n2. Practice stringy stuff." << endl;
 
     // return 0 to indicate the program terminated successfully...btw
     return 0;
 }
-
-
-
-
-
-
-// function used to read from "filename" and output its data to stdout
-bool readFileToStdout() {
-    cout << "Here are some fun facts about Lake Powell: " << endl;
-
-    // open file stream
-    ifstream input;
-    input.open(POWELL_FACTS_FILENAME, ofstream::out | ofstream::app);
-    if (input.fail()) {
-        cerr << "Failed to open the \"" << POWELL_FACTS_FILENAME << "\" file to read." << endl;
-        return false;
-    }
-
-    // output file contents
-    string fact;
-    int count = 1;
-    while (getline(input, fact)) {  // can also do while (!input.eof())
-        cout << "\tFACT " << count << " - " << fact << endl;
-        count++;
-    }
-
-    cout << "Reached end of file." << endl << endl;
-    return true;
-}
-
-
-// function that asks for user input and writes it to a file
-bool printStdinToFile() {
-    cout << "\nTell me your favorite joke and I will add it to my list of jokes! Press enter when done..." << endl;
-    string user_joke;
-    getline(cin, user_joke);
-
-    ofstream output;
-    output.open(JOKES_FILENAME);
-    if (output.fail()) {
-        cerr << "Failed to open the \"" << JOKES_FILENAME << "\" file to write." << endl;
-        return false;
-    }
-
-    output << user_joke;
-    output.close();
-    cout << "Thanks! Your joke has been added!" << endl;
-    return true;
-}
-
-
