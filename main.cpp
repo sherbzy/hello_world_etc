da37e284a2d42e181cfa52bf2da46b208c40046d
sherbzy <lsherburne@mines.edu>
2023-08-06 12:38:39 -0700

    Initial commit - create project and add description

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Hello World, etc.
+ *
+ *  Author: Lauren Sherburne
+ *  Date: 08/06/2023
+ *  Description: This is just a dummy check to make sure I can use clion its debugger and know
+ *  the c++ syntax for some standard things like file input/output, etc.
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    // return 0 to indicate the program terminated successfully...btw
+    return 0;
+}
